3994d6feaf28078ce215ef541ca39b3387cd45f7
Eric Lombrozo <elombrozo@gmail.com>
2014-06-17 19:05:08 -0700

    Partial movement of commands to their own functions.

--- src/main.cpp
+++ src/main.cpp
@@ -45,9 +45,13 @@ std::string getAddressFromScript(const bytes_t& script, const unsigned char base
         return "N/A";
 }
 
+// Server commands
 json_spirit::Value cmd_status(CoinDB::Vault* vault, const json_spirit::Array& params)
 {
     using namespace json_spirit;
+
+    if (params.size() != 0)
+        throw std::runtime_error("Invalid parameters.");
 
     Object result;
     result.push_back(Pair("name", vault->getName()));
@@ -55,6 +59,319 @@ json_spirit::Value cmd_status(CoinDB::Vault* vault, const json_spirit::Array& pa
     result.push_back(Pair("horizon", (uint64_t)vault->getHorizonTimestamp()));
     return result;
 }
+
+json_spirit::Value cmd_newkeychain(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() != 1 || params[0].type() != str_type)
+        throw std::runtime_error("Invalid parameters.");
+
+    std::string keychainName = params[0].get_str();
+    vault->newKeychain(keychainName, random_bytes(32));
+    return Value("success");
+}
+
+json_spirit::Value cmd_renamekeychain(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() != 2 || params[0].type() != str_type || params[0].type != str_type)
+        throw std::runtime_error("Invalid parameters.");
+
+    std::string oldName = params[0].get_str();
+    std::string newName = params[1].get_str();
+    vault->renameKeychain(oldName, newName);
+    return Value("success");
+}
+
+json_spirit::Value cmd_keychaininfo(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() != 1 || params[0].type() != str_type)
+        throw std::runtime_error("Invalid parameters.");
+
+    std::string keychainName = params[0].get_str();
+    std::shared_ptr<Keychain> keychain = vault->getKeychain(keychainName);
+    Object keychainInfo;
+    keychainInfo.push_back(Pair("id", (uint64_t)keychain->id()));
+    keychainInfo.push_back(Pair("name", keychain->name()));
+    keychainInfo.push_back(Pair("depth", (int)keychain->depth()));
+    keychainInfo.push_back(Pair("parentfp", (uint64_t)keychain->parent_fp()));
+    keychainInfo.push_back(Pair("childnum", (uint64_t)keychain->child_num()));
+    keychainInfo.push_back(Pair("pubkey", uchar_vector(keychain->pubkey()).getHex()));
+    keychainInfo.push_back(Pair("hash", uchar_vector(keychain->hash()).getHex()));
+    return keychainInfo;
+}
+
+json_spirit::Value cmd_keychains(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() > 2)
+        throw std::runtime_error("Invalid parameters.");
+
+    std::string accountName;
+    if (params.size() > 0) accountName = params[0].get_str();
+
+    bool showHidden = params.size() > 1 && params[1].get_bool();
+
+    vector<KeychainView> views = vault->getRootKeychainViews(accountName, showHidden);
+    vector<Object> keychainObjects;
+    for (auto& view: views)
+    {
+        Object obj;
+        obj.push_back(Pair("id", (uint64_t)view.id));
+        obj.push_back(Pair("name", view.name));
+        obj.push_back(Pair("private", view.is_private));
+        obj.push_back(Pair("encrypted", view.is_encrypted));
+        obj.push_back(Pair("locked", view.is_locked));
+        obj.push_back(Pair("hash", uchar_vector(view.hash).getHex()));
+        keychainObjects.push_back(obj);
+    }
+
+    Object result;
+    result.push_back(Pair("keychains", Array(keychainObjects.begin(), keychainObjects.end())));
+    return result;
+}
+
+json_spirit::Value cmd_exportbip32(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() != 1)
+        throw std::runtime_error("Invalid parameters.");
+
+    std::string keychainName = params[0].get_str();
+
+    vault->unlockChainCodes(secure_bytes_t()); // TODO: add a method to unlock chaincodes
+    bytes_t extendedkey = vault->getKeychainExtendedKey(keychainName, false);
+
+    Object result;
+    result.push_back(Pair("extendedkey", toBase58Check(extendedkey)));
+    return result;
+}
+
+json_spirit::Value cmd_importbip32(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() != 2)
+        throw std::runtime_error("Invalid parameters.");
+
+    bytes_t extendedkey;
+    if (!fromBase58Check(params[1].get_str(), extendedkey))
+        throw std::runtime_error("Invalid extended key.");
+
+    vault->importKeychainExtendedKey(params[0].get_str(), extendedkey, false);
+    return Value("success");
+}
+
+json_spirit::Value cmd_newaccount(CoinDB::Vault* vault, const json_spirit::Array& params)
+{
+    using namespace json_spirit;
+
+    if (params.size() < 3 || params.size() > 18)
+        throw std::runtime_error("Invalid parameters.");
+
+    std::string accountName = params[0].get_str();
+    int minsigs = params[1].get_int();
+    if (minsigs > ((int)params.size() - 2))
+        throw std::runtime_error("Invalid parameters.");
+
+    std::vector<std::string> keychainNames;
+    for (size_t i = 2; i < params.size(); i++)
+        keychainNames.push_back(params[i].get_str());
+
+    vault->unlockChainCodes(secure_bytes_t()); // TODO: add a method to unlock chaincodes using passphrase
+    vault->newAccount(accountName, minsigs, keychainNames);
+    vault->lockChainCodes();
+    return Value("success");
+}
+/*
+        else if (method == "renameaccount")
+        {
+            if (params.size() != 2)
+                throw std::runtime_error("Invalid parameters.");
+
+            std::string oldName = params[0].get_str();
+            std::string newName = params[1].get_str();
+            vault->renameAccount(oldName, newName);
+            response.setResult("success", id);
+        }
+        else if (method == "accountinfo")
+        {
+            if (params.size() != 1)
+                throw std::runtime_error("Invalid parameters.");
+
+            std::string accountName = params[0].get_str();
+            AccountInfo accountInfo = vault->getAccountInfo(accountName);
+            uint64_t balance = vault->getAccountBalance(accountName, 0);
+            uint64_t confirmedBalance = vault->getAccountBalance(accountName, 1);
+
+            Object result;
+            result.push_back(Pair("id", (uint64_t)accountInfo.id()));
+            result.push_back(Pair("name", accountInfo.name()));
+            result.push_back(Pair("minsigs", (int)accountInfo.minsigs()));
+            result.push_back(Pair("keychains", Array(accountInfo.keychain_names().begin(), accountInfo.keychain_names().end())));
+            result.push_back(Pair("unusedpoolsize", (uint64_t)accountInfo.unused_pool_size()));
+            result.push_back(Pair("timecreated", (uint64_t)accountInfo.time_created()));
+            result.push_back(Pair("bins", Array(accountInfo.bin_names().begin(), accountInfo.bin_names().end())));
+            result.push_back(Pair("balance", balance));
+            result.push_back(Pair("confirmedbalance", confirmedBalance));
+            response.setResult(result, id); 
+        }
+        else if (method == "listaccounts")
+        {
+            if (params.size() > 0)
+                throw std::runtime_error("Invalid parameters.");
+
+            vector<AccountInfo> accounts = vault->getAllAccountInfo();
+            vector<Object> accountObjects;
+            for (auto& account: accounts)
+            {
+                Object accountObject;
+                accountObject.push_back(Pair("id", (uint64_t)account.id()));
+                accountObject.push_back(Pair("name", account.name()));
+                accountObject.push_back(Pair("minsigs", (int)account.minsigs()));
+
+                accountObject.push_back(Pair("keychains", Array(account.keychain_names().begin(), account.keychain_names().end())));
+
+                accountObjects.push_back(accountObject);
+            }
+            Object result;
+            result.push_back(Pair("accounts", Array(accountObjects.begin(), accountObjects.end())));
+            response.setResult(result, id);
+        }
+        else if (method == "issuescript")
+        {
+            if (params.size() < 1 || params.size() > 3)
+                throw std::runtime_error("Invalid parameters.");
+
+            std::string accountName = params[0].get_str();
+            std::string label;
+            if (params.size() > 1) label = params[1].get_str();
+            std::string binName;
+            if (params.size() > 2) binName = params[2].get_str();
+            if (binName.empty()) binName = DEFAULT_BIN_NAME;
+
+            std::shared_ptr<SigningScript> script = vault->issueSigningScript(accountName, binName, label);
+
+            std::string address = getAddressFromScript(script->txoutscript(), BITCOIN_BASE58_VERSIONS);
+            std::string uri = "bitcoin:";
+            uri += address;
+            if (!label.empty()) { uri += "?label="; uri += label; }
+
+            Object result;
+            result.push_back(Pair("account", accountName));
+            result.push_back(Pair("label", label));
+            result.push_back(Pair("accountbin", binName));
+            result.push_back(Pair("script", uchar_vector(script->txoutscript()).getHex()));
+            result.push_back(Pair("address", address));
+            result.push_back(Pair("uri", uri)); 
+            response.setResult(result, id);
+        }
+        else if (method == "txs")
+        {
+            if (params.size() > 1)
+                throw std::runtime_error("Invalid parameters.");
+
+            uint32_t minheight = params.size() > 0 ? (uint32_t)params[0].get_uint64() : 0;
+            std::vector<TxView> txviews = vault->getTxViews(Tx::ALL, 0, -1, minheight);
+
+            std::vector<Object> txViewObjs; 
+            for (auto& txview: txviews)
+            {
+                txViewObjs.push_back(getTxViewObject(txview));
+            }
+
+            Object result;
+            result.push_back(Pair("txs", Array(txViewObjs.begin(), txViewObjs.end())));
+            response.setResult(result, id);
+        }
+        else if (method == "gettx")
+        {
+            if (params.size() != 1)
+                throw std::runtime_error("Invalid parameters.");
+
+            std::shared_ptr<Tx> tx;
+            if (params[0].type() == str_type)
+            {
+                uchar_vector hash(params[0].get_str());
+                tx = vault->getTx(hash); 
+            }
+            else
+            {
+                tx = vault->getTx((unsigned long)params[0].get_uint64());
+            }
+
+            Value txObj;
+            if (!read_string(tx->toJson(), txObj))
+                throw std::runtime_error("Internal error - invalid tx json.");
+
+            Object result;
+            result.push_back(Pair("tx", txObj));
+            result.push_back(Pair("hex", uchar_vector(tx->raw()).getHex()));
+            response.setResult(result, id);
+        }
+        else if (method == "blockheader")
+        {
+            if (params.size() != 1)
+                throw std::runtime_error("Invalid parameters.");
+
+            uint32_t height = (uint32_t)params[0].get_uint64();
+            std::shared_ptr<BlockHeader> header = vault->getBlockHeader(height);
+            response.setResult(getBlockHeaderObject(header.get()), id);
+        }
+        else if (method == "bestblockheader")
+        {
+            if (params.size() > 0)
+                throw std::runtime_error("Invalid parameters.");
+
+            uint32_t height = vault->getBestHeight();
+            std::shared_ptr<BlockHeader> header = vault->getBlockHeader(height);
+            response.setResult(getBlockHeaderObject(header.get()), id);
+        }
+        else if (method == "newtx")
+        {
+            if (params.size() < 3)
+                throw std::runtime_error("Invalid parameters.");
+
+            std::string account = params[0].get_str();
+
+            // Get outputs
+            size_t i = 1;
+            txouts_t txouts;
+            do
+            {
+                if (params[i].type() != str_type || params[i+1].type() != int_type)
+                    throw std::runtime_error("Invalid parameters.");
+
+                std::string address = params[i++].get_str();
+                uint64_t value = params[i++].get_uint64();
+                bytes_t txoutscript = CoinQ::Script::getTxOutScriptForAddress(address, BITCOIN_BASE58_VERSIONS);
+                std::shared_ptr<TxOut> txout(new TxOut(value, txoutscript));
+                txouts.push_back(txout);
+                 
+            } while (i < (params.size() - 1) && (params[i].type() == str_type));
+
+            uint64_t fee = i < params.size() ? params[i++].get_uint64() : 0;
+            uint32_t version = i < params.size() ? (uint32_t)params[i++].get_uint64() : 1;
+            uint32_t locktime = i < params.size() ? (uint32_t)params[i++].get_uint64() : 0;
+
+            std::shared_ptr<Tx> tx = vault->createTx(account, version, locktime, txouts, fee, 1, true);
+
+            Value txObj;
+            if (!read_string(tx->toJson(), txObj))
+                throw std::runtime_error("Internal error - invalid tx json.");
+
+            Object result;
+            result.push_back(Pair("tx", txObj));
+            result.push_back(Pair("hex", uchar_vector(tx->raw()).getHex()));
+            response.setResult(result, id);
+        }
+*/
 
 bool g_bShutdown = false;
 
@@ -99,12 +416,8 @@ void requestCallback(SynchedVault& synchedVault, WebSocket::Server& server, cons
         }
         else if (method == "newkeychain")
         {
-            if (params.size() != 1)
-                throw std::runtime_error("Invalid parameters.");
-
-            std::string keychainName = params[0].get_str();
-            vault->newKeychain(keychainName, random_bytes(32));
-            response.setResult("success", id);
+            Value result = cmd_newkeychain(vault, params);
+            response.setResult(result, id);
         }
         else if (method == "renamekeychain")
         {
