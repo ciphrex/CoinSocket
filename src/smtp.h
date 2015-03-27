5fe3af5ba3457ebc25abe70f0e1c66a73cd52a9c
Eric Lombrozo <elombrozo@gmail.com>
2015-03-26 19:57:18 -0700

    Added smtp unit.

---
+++ src/smtp.h
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// smtp.h
+//
+// Copyright (c) 2015 Eric Lombrozo
+//
+// All Rights Reserved.
+
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+
+class SmtpTls
+{
+public:
+    SmtpTls(const std::string& username, const std::string& password, const std::string& url) :
+        username_(username), password_(password), url_(url) { }
+
+    void setSubject(const std::string& subject) { subject_ = subject; }
+    void setBody(const std::string& body) { body_ = body; }
+    void setFrom(const std::string& from) { from_ = from; }
+    void addTo(const std::string& to) { tos_.push_back(to); }
+    void addCc(const std::string& cc) { ccs_.push_back(cc); }
+    void addBcc(const std::string& bcc) { bccs_.push_back(bcc); }
+
+    void send(bool verifycert = false, bool verbose = false) const;
+
+private:
+    std::string username_;
+    std::string password_;
+    std::string url_;
+
+    std::string from_;
+    std::string subject_;
+    std::string body_;
+    std::vector<std::string> tos_;
+    std::vector<std::string> ccs_;
+    std::vector<std::string> bccs_;
+
+    std::string payload_text_;
+};
