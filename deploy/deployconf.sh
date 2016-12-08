f88e801b13e9c51a2df78124749e14fd23651203
Eric Lombrozo <elombrozo@gmail.com>
2016-12-08 00:11:40 -0800

    Added support in deploy scripts for arbitrary user account under which coinsocketd runs.

--- deploy/deployconf.sh
+++ deploy/deployconf.sh
@@ -5,30 +5,29 @@ if (( $# != 2 )); then
     exit
 fi
 
-mkdir -p /home/coinsocket/.$1
-touch /home/coinsocket/.$1/coinsocket.conf
+mkdir -p ~/.$1
+touch ~/.$1/coinsocket.conf
 
-echo "network=bitcoin" > /home/coinsocket/.$1/coinsocket.conf
-echo "peerhost=localhost" >> /home/coinsocket/.$1/coinsocket.conf
-echo "sync=true" >> /home/coinsocket/.$1/coinsocket.conf
-echo '' >> /home/coinsocket/.$1/coinsocket.conf
+echo "network=bitcoin" > ~/.$1/coinsocket.conf
+echo "peerhost=localhost" >> ~/.$1/coinsocket.conf
+echo "sync=true" >> ~/.$1/coinsocket.conf
+echo '' >> ~/.$1/coinsocket.conf
 
-echo "dbname=$1" >> /home/coinsocket/.$1/coinsocket.conf
-echo "dbuser=" >> /home/coinsocket/.$1/coinsocket.conf
-echo "dbpasswd=" >> /home/coinsocket/.$1/coinsocket.conf
-echo '' >> /home/coinsocket/.$1/coinsocket.conf
+echo "dbname=$1" >> ~/.$1/coinsocket.conf
+echo "dbuser=" >> ~/.$1/coinsocket.conf
+echo "dbpasswd=" >> ~/.$1/coinsocket.conf
+echo '' >> ~/.$1/coinsocket.conf
 
-echo "wsport=$2" >> /home/coinsocket/.$1/coinsocket.conf
-echo "allowedips=.*" >> /home/coinsocket/.$1/coinsocket.conf
-echo "tlscertfile=" >> /home/coinsocket/.$1/coinsocket.conf
+echo "wsport=$2" >> ~/.$1/coinsocket.conf
+echo "allowedips=.*" >> ~/.$1/coinsocket.conf
+echo "tlscertfile=" >> ~/.$1/coinsocket.conf
 
 connectkey=$(openssl rand -base64 32 | head -c 32)
-echo "connectkey=$connectkey" >> /home/coinsocket/.$1/coinsocket.conf
-echo '' >> /home/coinsocket/.$1/coinsocket.conf
+echo "connectkey=$connectkey" >> ~/.$1/coinsocket.conf
+echo '' >> ~/.$1/coinsocket.conf
 
-echo "#emailalerts=" >> /home/coinsocket/.$1/coinsocket.conf
-echo "#emailalerts=" >> /home/coinsocket/.$1/coinsocket.conf
-echo "#smtpuser=" >> /home/coinsocket/.$1/coinsocket.conf
-echo "#smtppasswd=" >> /home/coinsocket/.$1/coinsocket.conf
-echo "#smtpurl=" >> /home/coinsocket/.$1/coinsocket.conf
-echo "#smtpfrom=<alerts@ciphrex.com>Ciphrex Alerts" >> /home/coinsocket/.$1/coinsocket.conf
+echo "#emailalerts=" >> ~/.$1/coinsocket.conf
+echo "#smtpuser=" >> ~/.$1/coinsocket.conf
+echo "#smtppasswd=" >> ~/.$1/coinsocket.conf
+echo "#smtpurl=" >> ~/.$1/coinsocket.conf
+echo "#smtpfrom=" >> ~/.$1/coinsocket.conf
--- deploy/deployupstart.sh
+++ deploy/deployupstart.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if (( $# != 1 )); then
+if (( $# != 2 )); then
     echo "Illegal number of parameters"
     exit
 fi
 
-echo "# Job for starting coinsocketd for $1 automatically" > /etc/init/coinsocketd-$1.conf
-echo '' >> /etc/init/coinsocketd-$1.conf
+echo "# Job for starting coinsocketd for $2 automatically" > /etc/init/coinsocketd-$2.conf
+echo '' >> /etc/init/coinsocketd-$2.conf
 
-echo 'author "Ciphrex"' >> /etc/init/coinsocketd-$1.conf
-echo 'description "coinsocketd-'$1' job"' >> /etc/init/coinsocketd-$1.conf
-echo '' >> /etc/init/coinsocketd-$1.conf
+echo 'author "Ciphrex"' >> /etc/init/coinsocketd-$2.conf
+echo 'description "coinsocketd-'$2' job"' >> /etc/init/coinsocketd-$2.conf
+echo '' >> /etc/init/coinsocketd-$2.conf
 
-echo 'stop on runlevel [!2345]' >> /etc/init/coinsocketd-$1.conf
-echo '#respawn' >> /etc/init/coinsocketd-$1.conf
-echo '' >> /etc/init/coinsocketd-$1.conf
+echo 'stop on runlevel [!2345]' >> /etc/init/coinsocketd-$2.conf
+echo '#respawn' >> /etc/init/coinsocketd-$2.conf
+echo '' >> /etc/init/coinsocketd-$2.conf
 
-echo 'script' >> /etc/init/coinsocketd-$1.conf
-echo "  sudo -u coinsocket coinsocketd --datadir $1" >> /etc/init/coinsocketd-$1.conf
-echo "end script" >> /etc/init/coinsocketd-$1.conf
+echo 'script' >> /etc/init/coinsocketd-$2.conf
+echo "  sudo -u $1 coinsocketd --datadir $2" >> /etc/init/coinsocketd-$2.conf
+echo "end script" >> /etc/init/coinsocketd-$2.conf
